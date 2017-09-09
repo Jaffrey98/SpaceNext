8647c8576af93779dee1d148604c8aa83e7bf289
AkashG6 <akashgarule@gmail.com>
2017-09-10 04:09:56 +0530

    changes

--- stu_reg.php
+++ stu_reg.php
@@ -11,12 +11,12 @@ $db = mysqli_select_db($con, DB_NAME) or die("Failed to connect to the database:
 
 function signup($con)
 {
-	echo "hello signup"."<br>";
+	// echo "hello signup"."<br>";
 	if (!empty($_POST['uname'])) {
-		echo "inside first if"."<br>";
+		// echo "inside first if"."<br>";
 		$query = mysqli_query($con, "SELECT * FROM reg_stud WHERE uname = '$_POST[uname]' AND pass = '$_POST[pass]'") or die(mysqli_error($con));
 		if (!$row = mysqli_fetch_array($query) or die(mysqli_error($con))) {
-			echo "inside second if"."<br>";
+			// echo "inside second if"."<br>";
 			NewUser($con);
 		}
 		else
@@ -29,19 +29,19 @@ function NewUser($con) {
 	$uname = $_POST['uname'];
 	$email = $_POST['email'];
 	$pass = $_POST['pass'];
-	echo "before insert declartion"."<br>";
+	// echo "before insert declartion"."<br>";
 	$query = "INSERT INTO reg_stud (uname, email_id , pass ,sch_eligible) VALUES ('$uname','$pass','$email',0)";
-	echo "after insert declartion"."<br>";
+	// echo "after insert declartion"."<br>";
 	$data = mysqli_query($con, $query) or die(mysqli_error($con));
-	echo "after insert query fired"."<br>";
+	// echo "after insert query fired"."<br>";
 	if($data){
-		echo "<p style='color:#F5F9F8;font-family:Verdana;'>Thank you for registering with us!</p>"."<br>";
+		echo "<p style='color:#F5F9F8;font-family:Verdana;'>Thank you for registering with us!</p>";
 	}
 }
 
 if (isset($_POST['submit'])) {
-	echo "entered isset func"."<br>";
+	// echo "entered isset func"."<br>";
 	signup($con);
-	echo "Sign up complete";
+	// echo "Sign up complete";
 }
 ?>
