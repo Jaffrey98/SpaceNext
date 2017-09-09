c155e613b554149ac0f544842d8a8cc1413a5cbd
Jaffrey Joy <jaffreyjoy@gmail.com>
2017-09-10 02:03:22 +0530

    updated stu_reg.php

---
+++ stu_reg.php
@@ -0,0 +1,47 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'spacenext');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Failed to connect to the database:".mysqli_error($con));
+
+$db = mysqli_select_db($con, DB_NAME) or die("Failed to connect to the database:".mysqli_error($con));
+
+function signup($con)
+{
+	echo "hello signup"."<br>";
+	if (!empty($_POST['username'])) {
+		$query = mysqli_query($con, "SELECT * FROM reg_stud WHERE userName = '$_POST[uname]' AND password = '$_POST[pass]'") or die(mysqli_error($con));
+
+		if (!$row = mysqli_fetch_array($query) or die(mysqli_error($con))) {
+			echo "inside second if"."<br>";
+			NewUser($con);
+		}
+		else
+			echo "Sorry, you are already registered."."<br>";
+	}
+}
+
+
+function NewUser($con) {
+	$uname = $_POST['uname'];
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];
+	echo "before insert declartion"."<br>";
+	$query = "INSERT INTO reg_stud (uname, email, password) VALUES ('$uname','$email','$pass')";
+	echo "after insert declartion"."<br>";
+	$data = mysqli_query($con, $query) or die(mysqli_error($con));
+	echo "after insert query fired"."<br>";
+	if($data){
+		echo "Thank you for registering with us!"."<br>";
+	}
+}
+
+if (isset($_POST['submit'])) {
+	echo "entered isset func"."<br>";
+	signup($con);
+	echo "Sign up complete";
+}
+?>
